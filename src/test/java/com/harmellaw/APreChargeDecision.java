b0da0acf15e53f42309e8c3c1590349e0115eb2f
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-11 16:04:35 +0100

    Mid-way through pomodoro

--- src/test/java/com/harmellaw/APreChargeDecision.java
+++ src/test/java/com/harmellaw/APreChargeDecision.java
@@ -5,13 +5,26 @@ import org.junit.jupiter.api.Test;
 
 public class APreChargeDecision {
 
+    PreChargeDecision myPreChargeDecision;
+
     @BeforeEach
     public void setup() {
+        myPreChargeDecision = new PreChargeDecision();
     }
+
+    // TODO: isRequestedByThePolice
+    // TODO: collectsAdvice
+    // TODO: collectsReminders
+    //
 
     @Test
     public void shouldRecordAlternativeCharge_AdviceAgainstSuspects() {
         // Make assertions
+
+        myPreChargeDecision.recordAlternateChargeAdvice(new Advice(currentCharge, alternateCharge, "This is the alternate charge advice"),
+                                                        new Suspect());
+
+        assertThatChargeAdviceWasRecorded
     }
     
     @Test
