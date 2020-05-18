7598a0fca45e085ec9ce292eb45143733096d0ee
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 15:04:35 +0100

    Implement ThePublicProsecutionService and CriminalCase attributes - compiles but test fails.

--- src/main/java/com/harmellaw/CriminalCase.java
+++ src/main/java/com/harmellaw/CriminalCase.java
@@ -1,4 +1,8 @@
 package com.harmellaw;
 
+import java.util.Set;
+
 public class CriminalCase {
+    public PNCId pncId;
+    public Set<Suspect> suspects;
 }
---
+++ src/main/java/com/harmellaw/PublicProsecutionService.java
@@ -0,0 +1,7 @@
+package com.harmellaw;
+
+public class PublicProsecutionService {
+    public CriminalCase receiveRequestForPreChargeDecision(PoliceInvestigation policeInvestigation) {
+        return null;
+    }
+}
