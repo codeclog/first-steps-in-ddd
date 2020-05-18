caf1974f5ae06032608c827a792a6ad40cfd7c29
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 16:18:24 +0100

    Add further insight - PoliceCaseFile is read-only.

--- src/main/java/com/harmellaw/preparation/PoliceCaseFile.java
+++ src/main/java/com/harmellaw/preparation/PoliceCaseFile.java
@@ -1,15 +1,14 @@
 package com.harmellaw.preparation;
 
 import com.harmellaw.PNCId;
-import com.harmellaw.preparation.Defendant;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class PoliceCaseFile {
 
-    public PNCId pncId;
-    public Set<Defendant> defendants = new HashSet<>();
+    public final PNCId pncId;
+    public final Set<Defendant> defendants = new HashSet<>();
 
     public PoliceCaseFile(PNCId pncId, Defendant defendant) {
         this.pncId = pncId;
