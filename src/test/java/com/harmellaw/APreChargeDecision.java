790371a01e5813aff2ff40d17f8c650d12dc7bc0
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 14:59:18 +0100

    Implement missing code - test passes.

--- src/test/java/com/harmellaw/APreChargeDecision.java
+++ src/test/java/com/harmellaw/APreChargeDecision.java
@@ -3,6 +3,8 @@ package com.harmellaw;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class APreChargeDecision {
 
     @BeforeEach
