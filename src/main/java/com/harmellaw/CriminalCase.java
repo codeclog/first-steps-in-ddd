aec78fb41f5b8a1b2f6d30d435096057cc70221e
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 15:54:22 +0100

    Break the current model - compilation failures.

--- src/main/java/com/harmellaw/CriminalCase.java
+++ src/main/java/com/harmellaw/CriminalCase.java
@@ -7,9 +7,10 @@ import java.util.Set;
 public class CriminalCase {
     public PNCId pncId;
     public Set<Suspect> suspects;
+    public Set<Defendant> defendants;
 
-    public CriminalCase(PNCId pncId, Set<Suspect> suspects) {
+    public CriminalCase(PNCId pncId, Set<Defendant> defendants) {
         this.pncId = pncId;
-        this.suspects = suspects;
+        this.defendants = defendants;
     }
 }
