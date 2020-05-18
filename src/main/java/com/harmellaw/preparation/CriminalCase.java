4b20fbadcc14d8fcc34c9d0686158fc671946fe1
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 16:38:55 +0100

    Remove unused Suspect attribute and dependency in CriminalCase.

--- src/main/java/com/harmellaw/preparation/CriminalCase.java
+++ src/main/java/com/harmellaw/preparation/CriminalCase.java
@@ -1,13 +1,12 @@
 package com.harmellaw.preparation;
 
 import com.harmellaw.PNCId;
-import com.harmellaw.investigation.Suspect;
 
 import java.util.Set;
 
 public class CriminalCase {
+    
     public PNCId pncId;
-    public Set<Suspect> suspects;
     public Set<Defendant> defendants;
 
     public CriminalCase(PNCId pncId, Set<Defendant> defendants) {
