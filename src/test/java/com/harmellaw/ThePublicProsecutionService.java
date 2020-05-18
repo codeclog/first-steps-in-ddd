06e9d26ff2ecaeb32e413c7850d4650876167e21
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 15:07:45 +0100

    Instantiate thePPS instance in fixture.

--- src/test/java/com/harmellaw/ThePublicProsecutionService.java
+++ src/test/java/com/harmellaw/ThePublicProsecutionService.java
@@ -1,5 +1,6 @@
 package com.harmellaw;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ThePublicProsecutionService {
 
     private PublicProsecutionService thePps;
+
+    @BeforeEach
+    public void setup() {
+        thePps = new PublicProsecutionService();
+    }
 
     @Test
     public void shouldCreateACaseWhenReceivingAPcdRequest() {
