329c043b99b08a02f47a931195741eade25103d2
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 16:37:22 +0100

    Remove unused imports in split Services.

--- src/main/java/com/harmellaw/investigation/PreChargeDecisionService.java
+++ src/main/java/com/harmellaw/investigation/PreChargeDecisionService.java
@@ -1,8 +1,5 @@
 package com.harmellaw.investigation;
 
-import com.harmellaw.investigation.PoliceInvestigationDetails;
-import com.harmellaw.investigation.PreChargeDecisionCase;
-
 public class PreChargeDecisionService {
     public PreChargeDecisionCase receiveRequestForPreChargeDecision(PoliceInvestigationDetails policeInvestigationDetails) {
         return new PreChargeDecisionCase(policeInvestigationDetails.pncId, policeInvestigationDetails.suspects);
--- src/main/java/com/harmellaw/preparation/TrialPreparationService.java
+++ src/main/java/com/harmellaw/preparation/TrialPreparationService.java
@@ -1,8 +1,5 @@
 package com.harmellaw.preparation;
 
-import com.harmellaw.preparation.CriminalCase;
-import com.harmellaw.preparation.PoliceCaseFile;
-
 public class TrialPreparationService {
     public CriminalCase acceptCaseFile(PoliceCaseFile policeCaseFile) {
         return new CriminalCase(policeCaseFile.pncId, policeCaseFile.defendants);
