f67006b69e5889067492474ec611c6dbec29a6d0
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-18 15:09:11 +0100

    Implement PublicProsecutionService.receiveRequestForPreChargeDecision() and add constructor to CriminalCase - all tests pass.

--- src/main/java/com/harmellaw/CriminalCase.java
+++ src/main/java/com/harmellaw/CriminalCase.java
@@ -5,4 +5,9 @@ import java.util.Set;
 public class CriminalCase {
     public PNCId pncId;
     public Set<Suspect> suspects;
+
+    public CriminalCase(PNCId pncId, Set<Suspect> suspects) {
+        this.pncId = pncId;
+        this.suspects = suspects;
+    }
 }
--- src/main/java/com/harmellaw/PublicProsecutionService.java
+++ src/main/java/com/harmellaw/PublicProsecutionService.java
@@ -2,6 +2,6 @@ package com.harmellaw;
 
 public class PublicProsecutionService {
     public CriminalCase receiveRequestForPreChargeDecision(PoliceInvestigation policeInvestigation) {
-        return null;
+        return new CriminalCase(policeInvestigation.pncId, policeInvestigation.suspects);
     }
 }
